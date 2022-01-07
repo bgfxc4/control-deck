583a76a9ba2a362d264b0c976db84a3bae40d267
bgfxc4 <bindermarc@gmx.de>
2022-01-07 23:28:27 +0100

    implemented spotify functionality with playerctl

--- main.ts
+++ main.ts
@@ -1,17 +1,11 @@
 import express from "express"
 import cors from "cors"
 import body_parser from "body-parser"
-const  connect = require("spotify-local-control")
+import { exec } from "child_process"
 
-export const app = express()
+const app = express()
 
 const main_server_port = 8080
-
-try {
-	var spotify_client = connect()
-} catch(e) {
-	console.log("Spotify not opened")
-}
 
 app.use(body_parser.json())
 app.use(cors())
@@ -19,16 +13,12 @@ app.use(cors())
 app.use("/", express.static("webclient"))
 
 app.get("/spotify/pause", (req, res) => {
-	if (!spotify_client)
-		return res.status(500).send("spotify_client not connected")
-	spotify_client.pause()
+	exec("playerctl --player=spotify pause")
 	res.send("ok")
 })
 
 app.get("/spotify/play", (req, res) => {
-	if (!spotify_client)
-		return res.status(500).send("spotify_client not connected")
-	spotify_client.resume()
+	exec("playerctl --player=spotify play")
 	res.send("ok")
 })
 
